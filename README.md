25f8a3a4b48ff4af943e2a748dab6b68b9e03342
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-08 13:03:37 +0530

    START: Test Maximum Program

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java_Day-13_Generics
---
+++ src/com/BridgeLabz/Generics/generics.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz.Generics;
+
+public class generics {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Test Maximum program");
+
+    }
+}
