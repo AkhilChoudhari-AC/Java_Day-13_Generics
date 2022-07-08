7645f132734a8e9fa584517038b847403570cd7f
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-08 14:40:18 +0530

    ADDED:Finding Maximum Among Three String Values

--- src/com/BridgeLabz/Generics/generics.java
+++ src/com/BridgeLabz/Generics/generics.java
@@ -5,21 +5,18 @@ public class generics {
     public static void main(String[] args) {
         System.out.println("Welcome to the Test Maximum program");
 
-        Float num1 = 5.3f;
-        Float num2 = 2.2f;
-        Float num3 = 8.4f;
+        String string1 = "Car";
+        String string2 = "Train";
+        String string3 = "Aeroplane";
 
-        if (num1.compareTo(num2) == 1 && num1.compareTo(num3) == 1) {
-            System.out.println("First number is maximum and number is : " + num1);
+        if (string1.compareTo(string2) == 1 && string1.compareTo(string3) == 1) {
+            System.out.println("First string is maximum and string is : " + string1);
 
-        } else if (num2.compareTo(num1) == 1 && num2.compareTo(num3) == 1) {
-            System.out.println("Second number is maximum and number is : " + num2);
+        } else if (string2.compareTo(string1) == 1 && string2.compareTo(string3) == 1) {
+            System.out.println("Second string is maximum and string is : " + string2);
 
         } else {
-            System.out.println("Third number is maximum and number is : " + num3);
+            System.out.println("Third string is maximum and string is : " + string3);
         }
-
     }
-
 }
-
